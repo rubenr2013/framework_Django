20bbbc75c7b8ee24b65a3dd3ccd8f3f12553adea
msaydr15 <rr2685346@gmail.com>
2025-07-16 11:59:09 -0500

    se crea la base  de datos y se agregan nuevos campos de informacion, tambien se inserta informacion

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+    gender = models.CharField(max_length=10, null=True)
+    birthday = models.DateField(null=True)
+    email = models.EmailField(max_length=255, null=True)
+    
