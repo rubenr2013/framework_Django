be3f311dee7057893313a7c12289dd4e4bac310a
msaydr15 <rr2685346@gmail.com>
2025-07-18 12:15:37 -0500

    creamos el panel de administrador, y hacemos visible la tabla de los usuarios, ademas se creo el usuario administrador, se eliminaron, agregamos y modificamos informacion de los usuarios.

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+
+
 def members(request):
     mymembers = Member.objects.all().values()
     template = loader.get_template("all_members.html")
@@ -8,6 +10,8 @@ def members(request):
         "mymembers": mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+
 def details(request, id):
     mymember = Member.objects.get(id=id)
     template = loader.get_template("details.html")
@@ -16,6 +20,15 @@ def details(request, id):
     }
     return HttpResponse(template.render(context, request))
 
+
 def main(request):
     template = loader.get_template("main.html")
     return HttpResponse(template.render())
+
+
+def testing(request):
+    template = loader.get_template("template.html")
+    context = {
+        "fruits": ["Apple", "Banana", "Cherry"],
+    }
+    return HttpResponse(template.render(context, request))
